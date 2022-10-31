6cd6e33ebbecf8fb0f8d3f115ec841754d768d2d
Kassiaana <116725454+Kassiaana@users.noreply.github.com>
2022-10-31 11:51:28 -0300

    Add files via upload

---
+++ javascript- kassiana/termometro.js
@@ -0,0 +1,10 @@
+var temperatura  = prompt("temperatura");
+temperatura = parseFloat(temperatura);
+if (temperatura>38){
+document.write("Está com febre");
+console.log("Está ccom febre");
+} else{
+    document.write("sem febre");
+    console.log ("sem febre");
+
+}
